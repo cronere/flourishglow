7caec7314963e67ec91a9a20b8ca16691d2d1718
cronere <merkley.jacob5@gmail.com>
2026-04-11 21:03:05 -0700

    Update generate-sample.js

--- pages/api/generate-sample.js
+++ pages/api/generate-sample.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end()
+  if (req.method !== 'POST') return res.status(405).end() 
 
   const { name, email, business_name, practice_type, services } = req.body
 
